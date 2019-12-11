8c5709cdcbaff3d29fe07957b4d5e0d7865741b3
TristanHoladay <tholaday777@gmail.com>
2019-12-10 22:25:00 -0600

    discriminator service created and works

--- src/app/modals/delete/delete.component.ts
+++ src/app/modals/delete/delete.component.ts
@@ -17,6 +17,7 @@ import { Iitem } from 'src/app/interfaces/iitem';
 import { IUserVehicles } from 'src/app/interfaces/user-vehicles';
 import { IjobTicket } from 'src/app/interfaces/ijob-ticket';
 import { IResourceType } from 'src/app/interfaces/resource-type';
+import { DiscriminatorService } from 'src/app/services/discriminator.service';
 
 @Component({
   selector: 'delete-modal',
@@ -29,64 +30,20 @@ export class DeleteModal implements OnInit {
   closeResult: string;
 
   constructor(
-    private userService: UserService,
-    private companyService: CompanyService,
-    private vehicleService: VehicleService,
-    private itemService: ItemService,
-    private typeService: ResourcetypeService,
-    private ticketService: UseticketService,
-    private requestService: InventoryrequestService,
-    private uservehiclesService: UservehiclesService,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private discService: DiscriminatorService
   ) { }
 
   ngOnInit() {
-    this.getObjectType(this.delObject);
+    this.discService.getObjectType(this.delObject);
+    this.service = this.discService.objectService;
   }
-
-  getObjectType(delObject: IUser | ICompany | Ivehicles | IRequest | Iitem | IUserVehicles | IjobTicket | IResourceType) {
-    if ("userT" in delObject) {
-      this.service = this.userService;
-    }
-
-    if ("compT" in delObject) {
-      this.service = this.companyService;
-    }
-
-    if ("vehicleT" in delObject) {
-      this.service = this.vehicleService;
-    }
-
-    if ("requestT" in delObject) {
-      this.service = this.requestService;
-    }
-
-    if ("itemT" in delObject) {
-      this.service = this.itemService;
-    }
-
-    if ("uvT" in delObject) {
-     this.service = this.uservehiclesService;
-    }
-
-    if ("ticketT" in delObject) {
-      this.service = this.ticketService;
-    }
-
-    if ("resourceTypeT" in delObject) {
-       this.service = this.typeService;
-    }
-  }
-
 
   deleteObject(delObject: any) {
     console.log(delObject.id);
     this.service.delete(delObject.id).subscribe(data => {
       console.log(data);
-    })
-    // this.service.delete(delObject.id).subscribe(data => {
-    //   console.log(data);
-    // });
+    });
   }
 
   open(content) {
---
+++ src/app/services/discriminator.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { IUser } from '../interfaces/iuser';
+import { ICompany } from '../interfaces/icompany';
+import { Ivehicles } from '../interfaces/ivehicles';
+import { IRequest } from '../interfaces/irequest';
+import { IUserVehicles } from '../interfaces/user-vehicles';
+import { Iitem } from '../interfaces/iitem';
+import { IjobTicket } from '../interfaces/ijob-ticket';
+import { IResourceType } from '../interfaces/resource-type';
+import { UserService } from './user.service';
+import { CompanyService } from './company.service';
+import { VehicleService } from './vehicle.service';
+import { ItemService } from './item.service';
+import { ResourcetypeService } from './resourcetype.service';
+import { UseticketService } from './useticket.service';
+import { InventoryrequestService } from './inventoryrequest.service';
+import { UservehiclesService } from './uservehicles.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DiscriminatorService {
+  objectService: any;
+
+  constructor(
+    private userService: UserService,
+    private companyService: CompanyService,
+    private vehicleService: VehicleService,
+    private itemService: ItemService,
+    private typeService: ResourcetypeService,
+    private ticketService: UseticketService,
+    private requestService: InventoryrequestService,
+    private uservehiclesService: UservehiclesService
+  ) { }
+
+  getObjectType(object: IUser | ICompany | Ivehicles | IRequest | Iitem | IUserVehicles | IjobTicket | IResourceType) {
+    if ("userT" in object) {
+      this.objectService = this.userService;
+    }
+
+    if ("compT" in object) {
+      this.objectService = this.companyService;
+    }
+
+    if ("vehicleT" in object) {
+      this.objectService = this.vehicleService;
+    }
+
+    if ("requestT" in object) {
+      this.objectService = this.requestService;
+    }
+
+    if ("itemT" in object) {
+      this.objectService = this.itemService;
+    }
+
+    if ("uvT" in object) {
+     this.objectService = this.uservehiclesService;
+    }
+
+    if ("ticketT" in object) {
+      this.objectService = this.ticketService;
+    }
+
+    if ("resourceTypeT" in object) {
+       this.objectService = this.typeService;
+    }
+  }
+}
