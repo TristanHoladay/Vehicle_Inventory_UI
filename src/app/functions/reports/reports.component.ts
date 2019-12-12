c30409839e5906b6c231f9c5a4687dad329b9af1
TristanHoladay <tholaday777@gmail.com>
2019-12-12 03:12:17 -0600

    starting reporting component

--- src/app/functions/reports/reports.component.ts
+++ src/app/functions/reports/reports.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTable, MatFormField, MatSortModule, MatSort } from '@angular/material';
+import { FormControl } from '@angular/forms';
+
+import { UserService } from 'src/app/services/user.service';
+import { CompanyService } from 'src/app/services/company.service';
+import { ItemService } from 'src/app/services/item.service';
+import { UseticketService } from 'src/app/services/useticket.service';
+import { InventoryrequestService } from 'src/app/services/inventoryrequest.service';
+import { VehicleService } from 'src/app/services/vehicle.service';
+import { ResourcetypeService } from 'src/app/services/resourcetype.service';
 
 @Component({
   selector: 'app-reports',
@@ -7,7 +17,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  //data source equals the data presented after user decides what data they want
+
+  constructor(
+    private userService: UserService,
+    private companyService: CompanyService,
+    private itemService: ItemService,
+    private ticketService: UseticketService,
+    private requestService: InventoryrequestService,
+    private vehicleService: VehicleService,
+    private rtService: ResourcetypeService
+  ) { }
 
   ngOnInit() {
   }
