1dcfdb33f2556c5031309c12c45fb5d1b9eba1d2
TristanHoladay <tholaday777@gmail.com>
2020-01-17 15:25:48 -0800

    further styled home page and added dynamic notifcations bar

--- src/app/admin-home-page/admin-home-page.component.ts
+++ src/app/admin-home-page/admin-home-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { VehicleService } from '../services/vehicle.service';
+import { InventoryrequestService } from '../services/inventoryrequest.service';
+import { Ivehicles } from '../interfaces/ivehicles';
+import { IRequest } from '../interfaces/irequest';
 
 @Component({
   selector: 'app-admin-home-page',
@@ -10,9 +14,46 @@ export class AdminHomePageComponent implements OnInit {
 showOptions: boolean = false;
 showGreeting: boolean = true;
 
-  constructor() { }
+vehicles: Ivehicles[] = [];
+requests: IRequest[] = [];
+
+  constructor(
+    private vehService: VehicleService,
+    private reqService: InventoryrequestService
+  ) { }
 
   ngOnInit() {
+    this.vehService.getAllVehicles().subscribe(data => {
+      this.vehicles = data;
+    });
+
+    this.reqService.getAllRequests().subscribe(data => {
+      this.requests = data;
+    });
+  }
+
+  vehNum(): number {
+    let vehNum = 0;
+
+    this.vehicles.forEach(function(veh) {
+      if(veh.status != "in") {
+        vehNum++;
+      }
+    });
+
+    return vehNum;
+  }
+
+  reqNum(): number {
+    let reqNum = 0;
+
+    this.requests.forEach(function(req) {
+      if(!req.complete) {
+        reqNum++;
+      }
+    });
+
+    return reqNum;
   }
 
 }
